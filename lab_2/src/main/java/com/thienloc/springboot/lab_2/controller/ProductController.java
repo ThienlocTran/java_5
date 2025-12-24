ced14d8ccd168d949062d2fcbe2b966387944fe6
ThienlocTran <tranthienloc21102005@gmail.com>
2025-12-24 09:07:13 +0700

    bai 4 lab 2

--- lab_2/src/main/java/com/thienloc/springboot/lab_2/controller/ProductController.java
+++ lab_2/src/main/java/com/thienloc/springboot/lab_2/controller/ProductController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/product")
 public class ProductController {
@@ -23,5 +26,30 @@ public class ProductController {
         model.addAttribute("name", product.getName());
         model.addAttribute("price", product.getPrice());
         return "form1";
+    }
+
+    @GetMapping("/form/bai4")
+    public String formBai4(Model model) {
+        Product p = new Product();
+        p.setName("iPhone 30");
+        p.setPrice(5000.0);
+        model.addAttribute("product", p);
+        model.addAttribute("items", getItems());
+        return "form_bai4";
+    }
+
+    @PostMapping("/save_bai4")
+    public String saveBai4(@RequestParam String name, @RequestParam Double price, Model model) {
+        Product product = new Product(name, price);
+        model.addAttribute("product", product);
+        model.addAttribute("items", getItems());
+        return "form_bai4";
+    }
+
+    public List<Product> getItems() {
+        return Arrays.asList(
+            new Product("A", 1000000.0),
+            new Product("B", 1200000.0)
+        );
     }
 }
---
+++ lab_2/src/main/java/com/thienloc/springboot/lab_2/controller/ResultController.java
@@ -0,0 +1,4 @@
+package com.thienloc.springboot.lab_2.controller;
+
+public class ResultController {
+}
