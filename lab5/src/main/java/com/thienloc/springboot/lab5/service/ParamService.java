56ba4d4c717c9bb82995ff81baba9d6efa0a5e1a
ThienlocTran <tranthienloc21102005@gmail.com>
2025-12-31 15:21:26 +0700

    bai 1 lab 5

---
+++ lab5/src/main/java/com/thienloc/springboot/lab5/service/ParamService.java
@@ -0,0 +1,67 @@
+package com.thienloc.springboot.lab5.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ParamService {
+    @Autowired
+    HttpServletRequest request;
+    /**
+     * Đọc chuỗi giá trị của tham số
+     * @param name tên tham số
+     * @param defaultValue giá trị mặc định
+     * @return giá trị tham số hoặc giá trị mặc định nếu không tồn tại
+     */
+    public String getString(String name, String defaultValue){...}
+/**
+ * Đọc số nguyên giá trị của tham số
+ * @param name tên tham số
+ * @param defaultValue giá trị mặc định
+ * @return giá trị tham số hoặc giá trị mặc định nếu không tồn tại*/
+ *
+
+    public int getInt(String name, int defaultValue) {...}
+ * /**
+  * * Đọc số thực giá trị của tham số
+  * * @param name tên tham số
+  * * @param defaultValue giá trị mặc định
+  * * @return giá trị tham số hoặc giá trị mặc định nếu không tồn tại
+  * */
+         *
+
+    public double getDouble(String name, double defaultValue) {...}
+ * /**
+  * * Đọc giá trị boolean của tham số
+  * * @param name tên tham số
+  * * @param defaultValue giá trị mặc định
+  * * @return giá trị tham số hoặc giá trị mặc định nếu không tồn tại
+  * */
+         *
+
+    public boolean getBoolean(String name, boolean defaultValue) {...}
+ * /**
+  * * Đọc giá trị thời gian của tham số
+  * * @param name tên tham số
+  * * @param pattern là định dạng thời gian
+  * * @return giá trị tham số hoặc null nếu không tồn tại
+  * * @throws RuntimeException lỗi sai định dạng
+  * */
+         *
+
+    public Date getDate(String name, String pattern) {...}
+ * /**
+  * * Lưu file upload vào thư mục
+  * * @param file chứa file upload từ client
+  * * @param path đường dẫn tính từ webroot
+  * * @return đối tượng chứa file đã lưu hoặc null nếu không có file upload
+  * * @throws RuntimeException lỗi lưu file
+  * */
+         *
+
+    public File save(MultipartFile file, String path) {...}
+ *
+
+}
---
+++ lab5/src/main/java/com/thienloc/springboot/lab5/service/SessionService.java
@@ -0,0 +1,26 @@
+package com.thienloc.springboot.lab5.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+    @Autowired
+    HttpSession session;
+    /**
+     * Đọc giá trị của attribute trong session
+     * @param name tên attribute
+     * @return giá trị đọc được hoặc null nếu không tồn tại
+     */
+    public <T> T get(String name) {...}
+    /**
+     * Thay đổi hoặc tạo mới attribute trong session
+     * @param name tên attribute
+     * @param value giá trị attribute
+     */
+    public void set(String name, Object value) {...}
+    /**
+     * Xóa attribute trong session
+     * @param name tên attribute cần xóa
+     */
+    public void remove(String name) {...}
+}
