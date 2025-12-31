8cf40ebdf7e1dff6aa5344c53d05b0dbf7bdf0a4
ThienlocTran <tranthienloc21102005@gmail.com>
2025-12-31 12:18:52 +0700

    finish bai 2 lab 4

--- lab_4/src/main/java/com/thienloc/springboot/lab3/controller/StaffController.java
+++ lab_4/src/main/java/com/thienloc/springboot/lab3/controller/StaffController.java
@@ -1,8 +1,10 @@
 package com.thienloc.springboot.lab3.controller;
 
 import com.thienloc.springboot.lab3.entity.Staff;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -73,11 +75,16 @@ public class StaffController {
         return "staff-create";
     }
     @RequestMapping("/staff/create/save")
-    public String createSave(Model model, @ModelAttribute("staff") Staff staff,
+    public String createSave(Model model, @Valid @ModelAttribute("staff") Staff staff, Errors errors,
                              @RequestPart("photo_file") MultipartFile photoFile) {
 // Gán tên file upload cho thuộc tính photo của bean nếu có upload file
         if(!photoFile.isEmpty()) {
             staff.setPhoto(photoFile.getName());
+        }
+        if(errors.hasErrors()){
+            model.addAttribute("message", "Vui lòng sửa các lỗi sau!");
+        }else{
+            model.addAttribute("message","Dữ liệu đã nhập đúng!");
         }
         model.addAttribute("message", "Xin chào " + staff.getName());
         return "staff-create";
--- lab_4/src/main/java/com/thienloc/springboot/lab3/entity/Staff.java
+++ lab_4/src/main/java/com/thienloc/springboot/lab3/entity/Staff.java
@@ -1,5 +1,6 @@
 package com.thienloc.springboot.lab3.entity;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,15 +13,25 @@ import java.util.Date;
 @AllArgsConstructor
 @Builder
 public class Staff {
+    @NotBlank(message = "chưa nhập email")
+    @Email(message = "Email chưa đúng định dạng")
     private String id;
+
+    @NotBlank(message = "Chưa nhập họ và tên")
     private String name;
+
     @Builder.Default
+    @NotNull(message = "Chưa chọn giới tính")
     private Boolean gender = true;
     @Builder.Default
     private String photo = "anhAIdo.jpg";
     @Builder.Default
+    @NotNull(message = "Chưa nhập ngày sinh")
+    @Past(message = "Ngày sinh không hợp lệ")
     private Date birthday = new Date();
     @Builder.Default
+    @Min(value = 1000,message = "Lương tối thiếu phải 1000")
+    @NotNull(message = "Chưa nhập lương")
     private Double salary = 1000000.0;
     @Builder.Default
     private Integer level = 1;
